6c8ac8f4c990ee0c59d3efe0227dfb0a64f29781
wu <rty21785@gmail.com>
2024-10-17 13:32:32 +0800

    feat(line): 跳過胖子

--- src/controllers/linebot.ts
+++ src/controllers/linebot.ts
@@ -213,6 +213,9 @@ const eventHandler = async (
     })
   }
 
+  // 跳過胖子
+  if (event.source!.userId == "U2d6c7cbcd3e3d089cd9295d5923602bc") return
+
   // get displayName
   const displayName = await getDisplayName(channelId, event.source)
 
